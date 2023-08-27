91b2800e3844e785cc2ac03cabf8446a4ac59b68
Bieu <133762855+Bieuxyz@users.noreply.github.com>
2023-08-27 15:46:30 +0700

    refactor: create team api (#11)
    * refactor: create team api
    
    * fix: swap to enum
    
    ---------
    
    Co-authored-by: phieu <phieu.yiu@gmail.com>

--- src/server/api/routers/team.ts
+++ src/server/api/routers/team.ts
@@ -1,6 +1,7 @@
 import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc"
 import { z } from "zod"
 import { zId } from "~/common/validation"
+import { Role } from "@prisma/client"
 
 export const createTeam = protectedProcedure
   .input(
@@ -11,21 +12,46 @@ export const createTeam = protectedProcedure
     })
   )
   .mutation(async ({ ctx, input }) => {
-    return ctx.prisma.team.create({
-      data: {
-        name: input.name,
-        description: input.description,
-        workspace: {
-          connect: {
-            name: input.workspaceName,
+    const userId = ctx.session.right.userId
+
+    return ctx.prisma.$transaction(async () => {
+      const team = await ctx.prisma.team.create({
+        data: {
+          name: input.name,
+          description: input.description,
+          workspace: {
+            connect: {
+              name: input.workspaceName,
+            },
+          },
+          owner: {
+            connect: {
+              id: ctx.session.right.userId,
+            },
           },
         },
-        owner: {
-          connect: {
-            id: ctx.session.right.userId,
+      })
+
+      return ctx.prisma.membersOnTeams.create({
+        data: {
+          role: Role.Owner,
+          workspace: {
+            connect: {
+              name: input.workspaceName,
+            },
+          },
+          team: {
+            connect: {
+              id: team.id,
+            },
+          },
+          user: {
+            connect: {
+              id: userId,
+            },
           },
         },
-      },
+      })
     })
   })
 
